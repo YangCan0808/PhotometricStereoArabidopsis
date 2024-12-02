87a01971a12bcef2aa8c622683ddb138bc0a059f
Can Yang <YangCan0808@outlook.com>
2024-12-02 21:31:06 +0000

    Refactor the main function.

--- photometric_stereo_reconstruction.py
+++ photometric_stereo_reconstruction.py
@@ -68,35 +68,48 @@ def calculate_normal_vector_matrix():
 
 
 def main():
+    # load config and images
     with open("config.json", "r") as config_file:
         config = json.load(config_file)
     images = load_images(config["image_paths"], config["roi_coordinates"])
-    segmented_images = hsv_segment(images, config["hsv_thresholds"])
 
+    # get mask
+    mask = generate_mask(config["mask_method"])
+
+    # grayscale conversion
     grayscale_images = []
-    for segmented_image in segmented_images:
-        grayscale_images.append(cv2.cvtColor(segmented_image, cv2.COLOR_BGR2GRAY))
+    for image in images:
+        grayscale_images.append(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+
+    # segment a ROI in grayscale images using a mask
+    segmented_images = []
+    for grayscale_image in grayscale_images:
+        segmented_images.append(
+            cv2.bitwise_and(grayscale_image, grayscale_image, mask=mask)
+        )
 
     # to np.array
-    images = np.stack(grayscale_images, axis=-1)
-
-    # light source directions
-    S = np.array(config["light_source_directions"])
-
+    images = np.stack(segmented_images, axis=-1)
     height, width, num_images = images.shape
-    # I.shape (num_images, height, width)
+
+    # get light source directions
+    S = get_light_source_directions(config["light_source_directions"])
+
+    # I.shape (num_images, height*width)
     I = images.reshape(-1, num_images).T
+
     # computing the normal vector matrix by Least Square Method
     N = np.linalg.lstsq(S, I, rcond=None)[0]
     zero_vectors = np.all(N == 0, axis=0)
     for i in range(N.shape[1]):
         if zero_vectors[i]:
             N[:, i] = [0, 0, 1]
+
     # normalize the normal vector matrix
     N = N / np.linalg.norm(N, axis=0)
     N = N.T.reshape(height, width, 3)
 
-    # Calculate depth map
+    # calculate depth map
     DEPTH_METHODS = {
         "poisson": depth_methods.calculate_depth_poisson,
         "integration": depth_methods.calculate_depth_integration,
